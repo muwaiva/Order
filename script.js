44679652a2814ae1ae5a5c3a37a22494d9290f44
muwaiva <163826004+muwaiva@users.noreply.github.com>
2024-03-24 11:02:22 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,113 +1,61 @@
-// let a=prompt("Введите число");
+let orderForm = document.getElementById("order-form");
+let orderTable = document.getElementById("order-table");
 
+orderForm.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-// function CheckA(){
-//     if (a>0) {
-//         console.log('chislo is positive');
-//     }
-//     else{
-//         console.log('chislo is negative')
-//     }
-// }
+    if (event.target['dish'].value.length > 3 &&
+     event.target['time'].value.length == 5 && 
+     event.target['address'].value.length > 5 && 
+     event.target['tel'].value.length > 11) {
+        let order = {
+            dish: event.target['dish'].value,
+            time: event.target['time'].value,
+            address: event.target['address'].value,
+            phone: event.target['tel'].value
+        }
+      
+        event.target.reset();
+       createOrder(order);
+    }
 
-// CheckA();
-
-
-
-
-
-
-
-// ////////////////////////////////////////
-
-
-
-
-
-
-
-// let b = "dhhjjngrghjfjfjfgfjghfgfigjfgf";
-
-// console.log( b.length);
+});
 
 
 
 
+function createOrder(newOrder) {
+    let tr = document.createElement("tr");
 
+    let dish = document.createElement("td");
+    dish.innerHTML = newOrder.dish;
+    let time = document.createElement("td");
+    time.innerHTML = newOrder.time;
+    let address = document.createElement("td");
+    address.innerHTML = newOrder.address;
+    let phone = document.createElement("td");
+    phone.innerHTML = newOrder.phone;
+    let link = document.createElement("td");
+    link.append(createLink(newOrder));
 
+    tr.append(dish);
+    tr.append(time);
+    tr.append(address);
+    tr.append(phone);
+    tr.append(link);
 
-// //////////////////////////////////////
+    orderTable.append(tr);
+}
 
-
-
-
-
-
-// let c ="dfhfgjhghfghfghfghfg";
-// let last = c.length - 1;
-// console.log(c[last]); 
-
-
-
-
-///////////////////////////////////
-
-
-
-
-//   let n = prompt("vvedite chislo");
-// function chetnoeOrnechet(){
-//     if(Math.floor(n / 2) == n / 2) {
-//         console.log('chislo is chet');
-       
-//     }
-//     else{
-//         console.log('chislo is nechet');
-     
-//     }
-// }
-
-// chetnoeOrnechet();
-
-
-
-
-////////////////////////////////
-
-
-
-// let s='abcd';
-// let c='abcd';
-
-// let l="a";
-// function pervayaBukva(){
-// if(s[l]==c[l]){
-//     console.log("true")
-// }
-// }
-// pervayaBukva();
-
-
-
-///////////////////////////////////
-
-// let m="ь";
-// let r=prompt("");
-// let last = r[r.length - 1];
-// function Znak(){
-//   if (last == m){
-//     console.log("ok")
-//   }
-//   else{ 
-//     console.log("neok")
-//   }
-// }
-
-// Znak();
-
-
-
-///////////////////////////////////////
+function createLink(newOrder) {
+    let text = JSON.stringify(newOrder);
+    let download = document.createElement("a");
+    download.setAttribute('href', 'data:text/plane;carset = utf-8,'
+        + encodeURIComponent(text));
+    download.setAttribute('download', 'order.json');
+    download.innerHTML = 'link';
+    return download;
+}
 
 
 
