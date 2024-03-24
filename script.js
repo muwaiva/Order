7b651d111a0a0d505c4383683ba39156daa4ac57
muwaiva <163826004+muwaiva@users.noreply.github.com>
2024-03-24 10:58:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,116 @@
+// let a=prompt("Введите число");
+
+
+// function CheckA(){
+//     if (a>0) {
+//         console.log('chislo is positive');
+//     }
+//     else{
+//         console.log('chislo is negative')
+//     }
+// }
+
+// CheckA();
+
+
+
+
+
+
+
+// ////////////////////////////////////////
+
+
+
+
+
+
+
+// let b = "dhhjjngrghjfjfjfgfjghfgfigjfgf";
+
+// console.log( b.length);
+
+
+
+
+
+
+
+// //////////////////////////////////////
+
+
+
+
+
+
+// let c ="dfhfgjhghfghfghfghfg";
+// let last = c.length - 1;
+// console.log(c[last]); 
+
+
+
+
+///////////////////////////////////
+
+
+
+
+//   let n = prompt("vvedite chislo");
+// function chetnoeOrnechet(){
+//     if(Math.floor(n / 2) == n / 2) {
+//         console.log('chislo is chet');
+       
+//     }
+//     else{
+//         console.log('chislo is nechet');
+     
+//     }
+// }
+
+// chetnoeOrnechet();
+
+
+
+
+////////////////////////////////
+
+
+
+// let s='abcd';
+// let c='abcd';
+
+// let l="a";
+// function pervayaBukva(){
+// if(s[l]==c[l]){
+//     console.log("true")
+// }
+// }
+// pervayaBukva();
+
+
+
+///////////////////////////////////
+
+// let m="ь";
+// let r=prompt("");
+// let last = r[r.length - 1];
+// function Znak(){
+//   if (last == m){
+//     console.log("ok")
+//   }
+//   else{ 
+//     console.log("neok")
+//   }
+// }
+
+// Znak();
+
+
+
+///////////////////////////////////////
+
+
+
+
+
+
